e7e58aae829e7855a329a04c9067a27beb0b5b16
d__ep__th <depth@localhost.localdomain>
2021-12-17 07:36:50 +0530

    Updating shard.yml for blockchain

--- src/blockchain.cr
+++ src/blockchain.cr
@@ -1,1 +1,22 @@
 require "digest"
+
+module Blockchain
+
+  class Block
+  
+    def initialize(data : String)
+      
+      @data = data
+      
+    end
+
+    def hash
+
+    hash = Digest::SHA256
+
+    end
+
+  end
+
+
+end
